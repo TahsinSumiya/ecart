d0c6b4bde88ce533d366ac3abed3fa6974360729
TahsinSumiya <tahsinadity187@gmail.com>
2022-02-15 17:27:02 +0600

    Initial commit

---
+++ ecart/shop/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index,name="shophome"),
+    path('about/', views.about,name="about"),
+     path('contact/', views.contact,name="contact"),
+      path('tracker/', views.tracker,name="tracker"),
+      path('checkout/', views.checkout,name="tracker"),
+      
+       path("products/<int:myid>", views.productview, name="ProductView"),
+]
